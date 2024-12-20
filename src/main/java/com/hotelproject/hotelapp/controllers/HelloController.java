241ff672cbe47e140173931b65fb7334f99b4bf6
achraf-bogryn <achrafbogryn12345@gmail.com>
2024-12-20 02:47:08 +0100

    create a controller for testing

--- src/main/java/com/hotelproject/hotelapp/controllers/HelloController.java
+++ src/main/java/com/hotelproject/hotelapp/controllers/HelloController.java
@@ -1,2 +1,13 @@
-package com.hotelproject.hotelapp.controllers;public class HelloController {
+package com.hotelproject.hotelapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }

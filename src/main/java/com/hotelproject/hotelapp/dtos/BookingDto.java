fc93c5180523633ad351aa7f4565e07ea1916965
achraf-bogryn <achrafbogryn12345@gmail.com>
2024-12-20 02:58:20 +0100

    add dto to my project

---
+++ src/main/java/com/hotelproject/hotelapp/dtos/BookingDto.java
@@ -0,0 +1,21 @@
+package com.hotelproject.hotelapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BookingDto {
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private UserDto user;
+    private RoomDto room;
+}
